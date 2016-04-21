5782b7ba701028370af4e58cd731daa41f0de72a
Jaken Herman <jch053@shsu.edu>
2016-04-21 15:57:42 -0500

    Update BSTMain.java

--- BSTMain.java
+++ BSTMain.java
@@ -1,21 +1,24 @@
 public class BSTMain{
     public static void main(String[] args){
 		BinarySearchTree bst = new BinarySearchTree();
-		bst.insert("I", "1");
-		bst.insert("C", "2");
-		bst.insert("B", "3");
-		bst.insert("D", "4");
-		bst.insert("E", "5");
-		bst.insert("F", "6");
-		bst.insert("G", "7");
-		bst.insert("H", "8");
-		bst.insert("A", "9");
+		bst.insert("Ajose", "295-1492");
+		bst.insert("Owens", "291-1864");
+		bst.insert("Federbush", "295-1601");
+		bst.insert("Ryba", "293-6122");
+		bst.insert("Seddon", "295-1882");
+		bst.insert("Copeland", "291-7890");
+		bst.insert("Voorhees", "294-8075");
+		bst.insert("Sparks", "584-3622");
 		
 		bst.inOrder();
         		System.out.println();
 
 		bst.preOrder();
 		System.out.println();
+        
+        System.out.println(bst.findCustomerIterative("Federbush"));
+        
+        System.out.println();
 
         System.out.println(bst.min());
 		System.out.println(bst.max());
